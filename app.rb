b63da29d18808f572b42d7f312916cf4d24ad32c
Aron Winston <aronwinston2792@gmail.com>
2019-01-14 22:13:38 -0500

    created my own sendgrid api key and value of email equals catalog.erb

--- app.rb
+++ app.rb
@@ -51,7 +51,7 @@ post "/contact" do
         @pumpkin_muffin = Muffin.new("Perfectly pump-a-licious", "$1.99/muffin", "pumpkin muffin", "https://bit.ly/2CmBlPu")
             ]
     
-    from = SendGrid::Email.new(email: 'aronwinston2792@gmail.com')
+    from = SendGrid::Email.new(email: 'awtheman@gmail.com')
     to = SendGrid::Email.new(email: params[:usr_email])
     subject = 'betsys bakery'
     content = SendGrid::Content.new(
@@ -64,7 +64,7 @@ post "/contact" do
     
     # sets up the api key
     sg = SendGrid::API.new(
-      api_key: ENV["SENDGRID_API_KEY"]
+      api_key: ENV["ARON_SENDGRID_API"]
     )
     
     # sends the email
